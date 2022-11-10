bf328b0b5ade0576728c88b6098a446b7e92f41f
Alex <san40_pan40magadan@mail.ru>
2022-11-10 13:43:24 +1100

    Test github-actions

--- test2.py
+++ test2.py
@@ -3,11 +3,10 @@ from payment import calculate as cal
 def test(a, b):
     c = cal(a, b)
 
-    test_c = a-b
-
-    assert  c == test_c
+    assert  c > 0
 
 
 test(10, 20)
 test(1, 15)
 test(40, 150)
+test(40, -150)
